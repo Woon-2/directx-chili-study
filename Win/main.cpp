ca6521f2af6afe06b28e2fb34ed64b654e5baea9
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-06-06 01:28:52 +0900

    created a basic window

--- Win/main.cpp
+++ Win/main.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <tchar.h>
 
 int CALLBACK WinMain(
     HINSTANCE hInstance,
@@ -7,6 +8,49 @@ int CALLBACK WinMain(
     [[maybe_unused]] int nCmdShow
 )
 {
-    while (true);
+    auto className = TEXT("hw3d");
+
+    auto wc = WNDCLASSEX();
+
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = static_cast<WNDPROC>(DefWindowProc);
+    wc.cbWndExtra = 0;
+    wc.cbClsExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = nullptr;
+    wc.hCursor = nullptr;
+    wc.hbrBackground = nullptr;
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = className;
+    wc.hIconSm = nullptr;  
+
+    RegisterClassEx(&wc);
+
+    auto hWnd = CreateWindowEx(
+        0,
+        className,
+        TEXT("WinMain"),
+        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+        200, 200, 640, 480, 
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
+
+    ShowWindow(hWnd, SW_SHOW);
+
+    // Message Loop
+
+    MSG msg;
+    BOOL result;
+
+    while ( ( result = GetMessage( &msg, hWnd, 0, 0 ) ) > 0 )
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
