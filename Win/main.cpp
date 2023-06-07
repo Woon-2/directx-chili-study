2b90a0d27b02d9f6baa7f54328d038e4432a1f6d
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-06-07 10:00:53 +0900

    made WndProc and tested WM_CLOSE message

--- Win/main.cpp
+++ Win/main.cpp
@@ -1,13 +1,22 @@
 #include <Windows.h>
 #include <tchar.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch( msg ) {
+    case WM_CLOSE:
+        PostQuitMessage(69);
+        break;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
     HINSTANCE hInstance,
     [[maybe_unused]] HINSTANCE hPrevInstance,
     [[maybe_unused]] LPSTR lpCmdLine,
     [[maybe_unused]] int nCmdShow
-)
-{
+) {
     // Window Creation
 
     auto className = TEXT("hw3d");
@@ -15,7 +24,7 @@ int CALLBACK WinMain(
 
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = static_cast<WNDPROC>(DefWindowProc);
+    wc.lpfnWndProc = static_cast<WNDPROC>(WndProc);
     wc.cbWndExtra = 0;
     wc.cbClsExtra = 0;
     wc.hInstance = hInstance;
@@ -48,11 +57,15 @@ int CALLBACK WinMain(
     auto msg = MSG();
     auto result = BOOL();
 
-    while ( ( result = GetMessage( &msg, hWnd, 0, 0 ) ) > 0 )
-    {
+    while ( ( result = GetMessage( &msg, hWnd, 0, 0 ) ) > 0 ) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    return 0;
+    if (result == -1) {
+        return -1;
+    }
+    else {
+        return static_cast<int>(msg.wParam);
+    }
 }
