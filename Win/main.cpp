d0f61464f9c3dab0e253a8d410743f47fbcecd5d
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-06-07 13:16:41 +0900

    modify window's title on WM_CHAR message

--- Win/main.cpp
+++ Win/main.cpp
@@ -13,6 +13,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     case WM_CLOSE:
         PostQuitMessage(69);
         break;
+
+    case WM_KEYUP:
+        if (wParam == 'F') {
+            SetWindowText(hWnd, TEXT("Danger field"));
+        }
+        break;
+
+    case WM_CHAR: {
+        static auto title = std::wstring();
+        title.push_back( static_cast<wchar_t>(wParam) );
+        SetWindowText(hWnd, title.c_str());
+        break;
+    }
     }
 
     return DefWindowProc(hWnd, msg, wParam, lParam);
