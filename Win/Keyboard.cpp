1438627443a593da6e9723976d7a33ce687775fd
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-06-12 21:15:03 +0900

    added keyboard event queue

--- Win/Keyboard.cpp
+++ Win/Keyboard.cpp
@@ -3,12 +3,13 @@
 #include <Windows.h>
 
 Keyboard::Keyboard(bool bListen) noexcept
-    : keys_(0u), listenKeys_(), bListen_(bListen)
+    : keys_(0u), listenKeys_(), bListen_(bListen), eventQueue_()
 {}
 
 Keyboard::Keyboard(std::set<Keyboard::KeyType>&& listenKeys,
     bool bListen) noexcept
-    : keys_(0u), listenKeys_( std::move(listenKeys) ), bListen_(bListen)
+    : keys_(0u), listenKeys_( std::move(listenKeys) ), bListen_(bListen),
+    eventQueue_()
 {}
 
 void Keyboard::scan()
@@ -55,4 +56,145 @@ constexpr void Keyboard::enableListening() noexcept
 constexpr void Keyboard::disableListening() noexcept
 {
     bListen_ = false;
+}
+
+Keyboard::EventQueue& Keyboard::getEventQueue() noexcept
+{
+    return eventQueue_;
+}
+
+const Keyboard::EventQueue& Keyboard::getEventQueue() const noexcept
+{
+    return eventQueue_;
+}
+
+Keyboard::Event::Event() noexcept
+    : key_(), state_(KeyState::Invalid)
+{}
+
+Keyboard::Event::Event(KeyType key, KeyState state) noexcept
+    : key_(key), state_(state)
+{}
+
+Keyboard::KeyType Keyboard::Event::getKey() const noexcept
+{
+    return key_;
+}
+
+Keyboard::KeyState Keyboard::Event::getState() const noexcept
+{
+    return state_;
+}
+
+Keyboard::EventQueue::EventQueue(bool bEnable)
+    : bActivate_(bEnable), bAutoRepeat_(false), keyQueue_(),
+    charQueue_()
+{}
+
+std::optional<Keyboard::Event> Keyboard::EventQueue::readKey()
+{
+    if (keyQueue_.size()) {
+        auto ev = keyQueue_.front();
+        keyQueue_.pop();
+        return ev;
+    }
+    else {
+        return std::nullopt;
+    }
+}
+
+std::optional<Keyboard::KeyType> Keyboard::EventQueue::readChar()
+{
+    if (charQueue_.size()) {
+        auto ch = charQueue_.front();
+        charQueue_.pop();
+        return ch;
+    }
+    else {
+        return std::nullopt;
+    }
+}
+
+bool Keyboard::EventQueue::keyEmpty() const
+{
+    return false;
+}
+
+bool Keyboard::EventQueue::charEmpty() const
+{
+    return false;
+}
+
+void Keyboard::EventQueue::flushKey()
+{
+    keyQueue_ = decltype(keyQueue_)();
+}
+
+void Keyboard::EventQueue::flushChar()
+{
+    charQueue_ = decltype(charQueue_)();
+}
+
+void Keyboard::EventQueue::flush()
+{
+    flushKey();
+    flushChar();
+}
+
+void Keyboard::EventQueue::enableAutoRepeat() noexcept
+{
+    bAutoRepeat_ = true;
+}
+
+void Keyboard::EventQueue::disableAutoRepeat() noexcept
+{
+    bAutoRepeat_ = false;
+}
+
+bool Keyboard::EventQueue::autoRepeatEnabled() const noexcept
+{
+    return bAutoRepeat_;
+}
+
+void Keyboard::EventQueue::onKeyPressed(KeyType key)
+{
+    if ( !enabled() ) {
+        return;
+    }
+    keyQueue_.push( Event(key, KeyState::Pressed) );
+    trimBuffer(keyQueue_);
+}
+
+void Keyboard::EventQueue::onKeyReleased(KeyType key)
+{
+    if ( !enabled() ) {
+        return;
+    }
+    keyQueue_.push( Event(key, KeyState::Released) );
+    trimBuffer(keyQueue_);
+}
+
+void Keyboard::EventQueue::onChar(KeyType ch)
+{
+    if ( !enabled() ) {
+        return;
+    }
+    charQueue_.push(ch);
+    trimBuffer(charQueue_);
+}
+
+void Keyboard::EventQueue::enable()
+{
+    bActivate_ = true;
+}
+
+void Keyboard::EventQueue::disable()
+{
+    bActivate_ = false;
+    flush();
+}
+
+bool Keyboard::EventQueue::enabled() const
+{
+    return bActivate_;
 }
--- Win/Keyboard.hpp
+++ Win/Keyboard.hpp
@@ -4,9 +4,10 @@
 #include <ranges>
 #include <bitset>
 #include <set>
-#include <limits>
 #include <type_traits>
 #include <algorithm>
+#include <queue>
+#include <optional>
 
 class Keyboard
 {
@@ -19,6 +20,79 @@ public:
         Invalid
     };
 
+    /*
+    Event Class' definition is here for complete type issue,
+    Keyboard Class has a EventQueue type member,
+    so to obtain Keyboard object's size, the compiler needs to know
+    EventQueue object's size.
+    this is why EventQueue Class' definition is in Keyboard Class,
+    and this is why Event Class' definition is in Keyboard Class.
+    */ 
+    class Event
+    {
+    public:
+        Event() noexcept;
+        Event(KeyType key, KeyState state) noexcept;
+        Event(const Event&) noexcept = default;
+        Event& operator=(const Event&) noexcept = default;
+        Event(Event&&) noexcept = default;
+        Event& operator=(Event&&) noexcept = default;
+
+        KeyType getKey() const noexcept;
+        KeyState getState() const noexcept;
+
+    private:
+        KeyType key_;
+        KeyState state_;
+    };
+private:
+    /*
+    EventQueue Class' definition is here for complete type issue,
+    Keyboard Class has a EventQueue type member,
+    so to obtain Keyboard object's size, the compiler needs to know
+    EventQueue object's size.
+    this is why EventQueue Class' definition is in Keyboard Class,
+    */ 
+    class EventQueue
+    {
+    private:
+        static constexpr size_t bufferSize = 16;
+
+    public:
+        EventQueue(bool bEnable = true);
+        EventQueue(const EventQueue&) = default;
+        EventQueue& operator=(const EventQueue&) = default;
+        EventQueue(EventQueue&&) noexcept = default;
+        EventQueue& operator=(EventQueue&&) noexcept = default;
+
+        std::optional<Event> readKey();
+        std::optional<KeyType> readChar();
+        bool keyEmpty() const;
+        bool charEmpty() const;
+        void flushKey();
+        void flushChar();
+        void flush();
+        void enableAutoRepeat() noexcept;
+        void disableAutoRepeat() noexcept;
+        bool autoRepeatEnabled() const noexcept;
+        template <class Buffer>
+        static void trimBuffer(Buffer& buffer);
+        static void trimBuffer(...);
+        void onKeyPressed(KeyType key);
+        void onKeyReleased(KeyType key);
+        void onChar(KeyType ch);
+        void enable();
+        void disable();
+        bool enabled() const;
+
+    private:
+        bool bActivate_;
+        bool bAutoRepeat_;
+        std::queue<Event> keyQueue_;
+        std::queue<KeyType> charQueue_;
+    };
+
+public:
     explicit Keyboard(bool bListen = true) noexcept;
     template <std::ranges::input_range R>
     explicit constexpr Keyboard(R&& listenKeys, bool bListen = true) noexcept(noexcept(
@@ -41,11 +115,14 @@ public:
     void embedListenKeys(std::set<KeyType>&& listenKeys) noexcept;
     constexpr void enableListening() noexcept;
     constexpr void disableListening() noexcept;
+    EventQueue& getEventQueue() noexcept;
+    const EventQueue& getEventQueue() const noexcept;
 
 private:
     std::bitset< std::numeric_limits<KeyType>::max() > keys_;
     std::set<KeyType> listenKeys_;
     bool bListen_;
+    EventQueue eventQueue_;
 };
 
 template <std::ranges::input_range R>
@@ -62,6 +139,19 @@ inline constexpr void Keyboard::embedListenKeys(R&& listenKeys) noexcept
 {
     std::copy( std::cbegin(listenKeys), std::cend(listenKeys),
         std::back_inserter(listenKeys_) );
+}
+
+template <class Buffer>
+void Keyboard::EventQueue::trimBuffer(Buffer& buffer)
+{
+    while (buffer.size() > bufferSize) {
+        buffer.pop();
+    }
+}
+
+void Keyboard::EventQueue::trimBuffer(...)
+{
+    // this shouldn't compile.
 }
 
 #endif  // __Keyboard
