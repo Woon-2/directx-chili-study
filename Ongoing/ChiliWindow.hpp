2ca4d2e8fe299a36db182d5d90bd3e09343ce258
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-10-10 23:08:08 +0900

    [feat](Ongoing): make skeleton of ChiliMsgHandler

--- Ongoing/ChiliWindow.hpp
+++ Ongoing/ChiliWindow.hpp
@@ -148,4 +148,42 @@ struct ChiliWindowTraits {
 using ChiliWindow = Win32::Window< Win32::ChiliWindowTraits<CHAR> >;
 using WChiliWindow = Win32::Window< Win32::ChiliWindowTraits<WCHAR> >;
 
+template <class Wnd>
+class BasicChiliMsgHandler : public Win32::MsgHandler<Wnd>{
+public:
+    using Win32::MsgHandler<Wnd>::window;
+    using MyChar = typename Win32::MsgHandler<Wnd>::MyChar;
+
+    std::optional<LRESULT> operator()(
+        const Win32::Message& msg
+    ) override {
+        try {
+            switch (msg.type) {
+            case WM_CLOSE:
+                PostQuitMessage(0);
+                return 0;
+
+            default:
+                break;
+            }
+        }
+        catch (const WindowException& e) {
+            MessageBoxA(nullptr, e.what(), "Window Exception",
+                MB_OK | MB_ICONEXCLAMATION);
+        }
+        catch (const std::exception& e) {
+            MessageBoxA(nullptr, e.what(), "Standard Exception",
+                MB_OK | MB_ICONEXCLAMATION);
+        }
+        catch(...) {
+            MessageBoxA(nullptr, "no details available",
+                "Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+        }
+
+        return {};
+    }
+private:
+
+};
+
 #endif  // __ChiliWindow
