5c14d548a3e8cb0c35df46774b2bbb13c066853c
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-10-13 23:48:48 +0900

    [feat](ong)(res): add resource path configuration

---
+++ resource/resource.hpp
@@ -0,0 +1,8 @@
+#ifndef __resource_paths
+#define __resource_paths
+
+#include <filesystem>
+
+const auto resource_path = std::filesystem::path("C:/Users/USER/Desktop/footprint of soilder/directx-chili-study/Resource");
+
+#endif
