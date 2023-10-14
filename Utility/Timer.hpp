18340f98649daf9d64ca0a967b41f246c445ff5d
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-10-14 22:11:09 +0900

    [feat](util): create timer functionalities

---
+++ Utility/Timer.hpp
@@ -0,0 +1,36 @@
+#ifndef __Timer
+#define __Timer
+
+#include <chrono>
+#include <ratio>
+
+template <class Rep, class Period = std::milli>
+class Timer {
+public:
+    using time_point = std::chrono::steady_clock::time_point;
+    using clock = std::chrono::steady_clock;
+    using duration = std::chrono::duration<Rep, Period>;
+
+    Timer() noexcept {
+        last_ = clock::now();
+    }
+
+    [[maybe_unused]] Rep mark() noexcept {
+        const auto old = last_;
+        last_ = clock::now();
+        const auto elapsed
+            = std::chrono::duration_cast<duration>(last - old);
+        return elapsed.count();
+    }
+
+    Rep peek() const noexcept {
+        return std::chrono::duration_cast<duration>(
+            clock::now() - last_
+        ).count();
+    }
+
+private:
+    time_point last_;
+};
+
+#endif  // __Timer
