4b3a496f933532a16d75991401b0a16cc39528d5
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-12-05 09:41:09 +0900

    [feat](ong): make box be fixed to mouse

--- Ongoing/src/Game/Box.cpp
+++ Ongoing/src/Game/Box.cpp
@@ -191,8 +191,9 @@ void Entity<Box>::update(milliseconds elapsed) {
             tc_->local().get()
             * dx::XMMatrixRotationY(0.7f)
             * dx::XMMatrixRotationX(-0.4f)
-            * dx::XMMatrixTranslation( mousePos.x, mousePos.y, 4.f )
+            * dx::XMMatrixTranslation( 0.f, 0.f, 4.f )
             * dx::XMMatrixPerspectiveLH( 1.f, 3.f/4.f, 0.5f, 10.f )
+            * dx::XMMatrixTranslation( mousePos.x, mousePos.y, 0.f )
         )
     );
     dc_->update(tc_->total());
