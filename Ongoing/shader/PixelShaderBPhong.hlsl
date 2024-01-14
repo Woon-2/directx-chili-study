81f50bfd9eb7e85e7d647038908413270f13bfdd
Woon2 <73771162+Woon-2@users.noreply.github.com>
2024-01-14 21:21:01 +0900

    [feat](ong): adopt cubed Half Rambert Shading

--- Ongoing/shader/PixelShaderBPhong.hlsl
+++ Ongoing/shader/PixelShaderBPhong.hlsl
@@ -27,7 +27,8 @@ float4 main( float3 worldPos : Position, float3 n : Normal ) : SV_Target {
     const float3 halfway = normalize(vToLDir + eyeDir);
 
     const float att = 1.0f / (attConst + attLin * vToLDist + attQuad * vToLDist * vToLDist);
-    const float3 diffuse = max( dot(vToLDir, n), 0 ) * diffuseColor;
+    const float rambertCoef = max(dot(vToLDir, n), 0);
+    const float3 diffuse = rambertCoef * diffuseColor;
     const float3 specular = pow( max( dot(halfway, n), 0 ), specularShinyness ) * specularColor;
     const float3 ambient = ambientColor;
 
