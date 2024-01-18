2b92ded83bfb94aebc2786bba0e94423ca7919b9
Woon2 <73771162+Woon-2@users.noreply.github.com>
2024-01-18 19:53:35 +0900

    [remove](ong): remove legacy primitive objects

--- Ongoing/src/Game/Game.cpp
+++ Ongoing/src/Game/Game.cpp
@@ -1,12 +1,5 @@
 #include "Game/Game.hpp"
 
-#include "Game/Box.hpp"
-#include "Game/Cone.hpp"
-#include "Game/Plane.hpp"
-#include "Game/Prism.hpp"
-#include "Game/Sphere.hpp"
-#include "Game/Sheet.hpp"
-#include "Game/SkinnedBox.hpp"
 #include "Game/IlluminatedBox.hpp"
 #include "Game/PointLight.hpp"
 
@@ -14,6 +7,7 @@
 #include "Game/GFXCMDLogGUIView.hpp"
 
 #include "AdditionalRanges.hpp"
+#include "GFX/Core/GraphicsNamespaces.hpp"
 
 #include "Image/GDIPlusMgr.hpp"
 // do as chili do, it's temporary.
@@ -34,24 +28,6 @@ Game::Game(const ChiliWindow& wnd, Graphics& gfx,
         dx::XMMatrixTranslation(0.f, 0.f, -20.f)
     );
     cameraControl_.show();
-
-    auto slotIndexedRender = rendererSystem_
-        .addRenderer<IndexedRenderer>();
-    rendererSystem_.enableLog(slotIndexedRender);
-    rendererSystem_.sync(slotIndexedRender);
-    rendererSystem_.scene(slotIndexedRender).setVision( camera_.vision() );
-
-    auto slotBlendedRender = rendererSystem_
-        .addRenderer<BlendedRenderer>();
-    rendererSystem_.enableLog(slotBlendedRender);
-    rendererSystem_.sync(slotBlendedRender);
-    rendererSystem_.scene(slotBlendedRender).setVision( camera_.vision() );
-
-    auto slotTexturedRender = rendererSystem_
-        .addRenderer<TexturedRenderer>();
-    rendererSystem_.enableLog(slotTexturedRender);
-    rendererSystem_.sync(slotTexturedRender);
-    rendererSystem_.scene(slotTexturedRender).setVision( camera_.vision() );
 
     auto slotBPhongRenderer = rendererSystem_
         .addRenderer<BPhongRenderer>();
@@ -76,16 +52,6 @@ Game::Game(const ChiliWindow& wnd, Graphics& gfx,
     pLight->viz().loader().loadAt(rendererSystem_.scene(slotSolidRenderer).layer(0));
 
     entities_.push_back(std::move(pLight));
-
-    // auto light = GFXMappedResource( GFXMappedResource::Type<BPDynPointLight>{},
-    //     rendererSystem_.storage(), gfx.factory(), rendererSystem_.storage()
-    // );
-    // light.as<BPDynPointLight>().setSlot(BPhongRenderer::slotLightCBuffer());
-    // light.as<BPDynPointLight>().coordSystem().setParent(coordSystem_);
-    // light.as<BPDynPointLight>().sync(camera_.vision());
-
-    // auto lScene = rendererSystem_.adapt<LSceneAdapter>(slotBPhongRenderer);
-    // lScene.addLight(std::move(light));
 
     inputSystem_.setListner(ic_);
 
@@ -135,114 +101,14 @@ void Game::render() {
 void Game::createObjects(std::size_t n, const ChiliWindow& wnd,
     Graphics& gfx, Keyboard<MyChar>& kbd, Mouse& mouse
 ) {
-    enum {
-        MAKE_BOX,
-        MAKE_CONE,
-        MAKE_PLANE,
-        MAKE_PRISM,
-        MAKE_SPHERE,
-        MAKE_SHEET,
-        MAKE_SKINNED_BOX,
-        MAKE_ILLUMINATED_BOX,
-        SIZE
-    };
-
-    auto distType = std::uniform_int_distribution<>(MAKE_ILLUMINATED_BOX, MAKE_ILLUMINATED_BOX);
-    auto distTesselation = std::uniform_int_distribution<std::size_t>(3u, 48u);
-
-    for (auto i = decltype(n)(0); i < n; ++i)  {
-        static constexpr auto pi = 3.14159f;
-
-    #define BASIC_DISTS \
-        Distribution(15.f, 6.f), /* distRadius */   \
-        Distribution(0.f, 2.f * pi), /* distCTP */    \
-        Distribution(0.03f * pi, 0.01f * pi), /* distDeltaCTP */    \
-        Distribution(0.5f * pi, 0.2f * pi) /* distDeltaRTY */
-
-        switch ( distType(rng) ) {
-        case MAKE_BOX:
-            createConcreteObject<Box>( BASIC_DISTS,
-                std::uniform_int_distribution<>(0, 1),
-                wnd, gfx, kbd, mouse
-            );
-            break;
-
-        case MAKE_CONE:
-            createConcreteObject<Cone>( BASIC_DISTS,
-                std::uniform_int_distribution<>(0, 1),
-                wnd, gfx, kbd, mouse,
-                distTesselation(rng)
-            );
-            break;
-
-        case MAKE_PLANE:
-            createConcreteObject<Plane>( BASIC_DISTS,
-                std::uniform_int_distribution<>(0, 1),
-                wnd, gfx, kbd, mouse,
-                distTesselation(rng), distTesselation(rng)
-            );
-            break;
-
-        case MAKE_PRISM:
-            createConcreteObject<Prism>( BASIC_DISTS,
-                std::uniform_int_distribution<>(0, 1),
-                wnd, gfx, kbd, mouse,
-                distTesselation(rng)
-            );
-            break;
-
-        case MAKE_SPHERE:
-            createConcreteObject<Sphere>( BASIC_DISTS,
-                std::uniform_int_distribution<>(0, 1),
-                wnd, gfx, kbd, mouse,
-                distTesselation(rng), distTesselation(rng)
-            );
-            break;
-
-        case MAKE_SHEET:
-            createConcreteObject<Sheet>( BASIC_DISTS,
-                std::uniform_int_distribution<>(2, 2),
-                wnd, gfx, kbd, mouse
-            );
-            break;
-
-        case MAKE_SKINNED_BOX:
-            createConcreteObject<SkinnedBox>( BASIC_DISTS,
-                std::uniform_int_distribution<>(2, 2),
-                wnd, gfx, kbd, mouse
-            );
-            break;
-
-        case MAKE_ILLUMINATED_BOX:
-            createIlBox( BASIC_DISTS, wnd, gfx, kbd, mouse );
-            break;
-
-        default:
-            break;
-        }
-
+    while (n--) {
+        createIlBox( Distribution(15.f, 6.f), /* distRadius */   \
+            Distribution(0.f, dx::XM_2PI), /* distCTP */    \
+            Distribution(0.03f * dx::XM_PI, 0.01f * dx::XM_PI), /* distDeltaCTP */    \
+            Distribution(dx::XM_PIDIV2, 0.2f * dx::XM_PI) /* distDeltaRTY */,
+            wnd, gfx, kbd, mouse
+        );
     }
-}
-
-template <class T, class ... TesselationFactors>
-void Game::createConcreteObject( Distribution distRadius,
-    Distribution distCTP,  Distribution distDeltaCTP,
-    Distribution distDeltaRTY, std::uniform_int_distribution<> distScene,
-    const ChiliWindow& wnd, Graphics& gfx,
-    Keyboard<MyChar>& kbd, Mouse& mouse,
-    TesselationFactors&& ... tesselationFactors
-) {
-    auto obj = std::make_unique<Entity<T>>();
-
-    obj->ctDrawComponent( gfx.factory(), gfx.pipeline(),
-        rendererSystem_.storage(), wnd,
-        std::forward<TesselationFactors>(tesselationFactors)...
-    );
-    obj->ctTransformComponent(distRadius, distCTP, distDeltaCTP, distDeltaRTY);
-
-    obj->loader().loadAt(rendererSystem_.scene( distScene(rng) ));
-
-    entities_.push_back( std::move(obj) );
 }
 
 void Game::createIlBox( Distribution distRadius,
@@ -257,7 +123,7 @@ void Game::createIlBox( Distribution distRadius,
     );
     obj->ctTransformComponent(distRadius, distCTP, distDeltaCTP, distDeltaRTY);
 
-    obj->loader().loadAt( rendererSystem_.adapt<LSceneAdapter>(3u) );
+    obj->loader().loadAt( rendererSystem_.adapt<LSceneAdapter>(0u) );
 
     entities_.push_back( std::move(obj) );
 }
